32f81f34916f2499b146f25ebea00d9ffee8064d
Christy Grimaud <christy.grimaud@gmail.com>
2020-06-23 13:28:13 -0400

    testing json with fetch

--- file.js
+++ file.js
@@ -1,24 +1,19 @@
 
 
-var rooms = [];
-    
-    function loadJSON(callback) {
-        var xobj = new XMLHttpRequest();
-        xobj.open('GET', 'rooms.json', true); 
-        xobj.onreadystatechange = function () {
-            if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-            }
-        };
-        xobj.send(null);
+
+
+fetch('rooms.json')
+    .then(function(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
     }
-
-    function init() {
-        loadJSON(function(response) {
-            rooms = JSON.parse(response)
-            console.log(rooms)
-
-        });
-    }
-
-init()
+        // Read the response as json.
+        return response.json();
+    })
+    .then(function(responseAsJson) {
+      // Do stuff with the JSON
+        console.log(responseAsJson);
+    })
+    .catch(function(error) {
+        console.log('Looks like there was a problem: \n', error);
+    });
