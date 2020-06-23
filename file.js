4fc7ebc1cbf33e98f77c7e59af180f37972a43ab
Christy Grimaud <christy.grimaud@gmail.com>
2020-06-23 15:11:22 -0400

    another attempt at reading json

--- file.js
+++ file.js
@@ -1,7 +1,15 @@
 
 
-$.getJSON( "rooms.json", function( data ) {
-    rooms = JSON.parse(data)
-    console.log(rooms)
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4) {
+        // const rooms = JSON.parse(xhr.responseText);
+        console.log(typeof xhr.responseText)
+        }
+}
 
-});
+
+xhr.open('GET', 'rooms.json');
+xhr.send();
+
+
