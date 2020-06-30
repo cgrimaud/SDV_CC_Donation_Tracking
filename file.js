f3bd6fe01534bba67dd1922ace3f113a4cb38a75
Christy Grimaud <christy.grimaud@gmail.com>
2020-06-30 11:29:02 -0400

    added item images to table

--- file.js
+++ file.js
@@ -23,20 +23,23 @@ $(document).ready( function () {
     function initTable() {
         $('#table_id').dataTable( {
             select: true,
-            scrollX: true,
             scrollY: "550px",
             scrollCollapse: true,
             paging: false,
             data: items,
             columns: [
                 {data: 'completed'}, 
+                {data: 'image',
+                render: function(data, type, row) {
+                    return '<img src="'+data+'" />';
+                    }
+                },
                 {data: 'name'},
                 {data: 'bundle'},
                 {data: 'room'}
             ],
             columnDefs: [
                 {"className": "dt-center", "targets": "_all"},
-                { width: '5%', targets: 0 },
             ],
             
         })
@@ -44,7 +47,7 @@ $(document).ready( function () {
         let completedColumn = $("tr td:nth-child(1)")
         completedColumn.replaceWith('<th style="text-align: center"><input type="checkbox" class:"checkbox" /></th>')
 
-        let itemColumn = $("tr td:nth-child(2)")
+        let itemColumn = $("tr td:nth-child(3)")
         var popover = itemColumn.attr({"data-toggle":"popover"})
         $('[data-toggle="popover"]').popover({
             placement : 'right',
@@ -63,15 +66,14 @@ $(document).ready( function () {
             });
         }
         
-
-
         addLocationTextToPopover()
         
     }
         
     class Item {
-        constructor(completed, item, bundle, room, id, location) {
+        constructor(completed, image, item, bundle, room, id, location) {
             this.completed = completed
+            this.image = image
             this.name = item;
             this.bundle = bundle;
             this.room = room;
@@ -97,7 +99,7 @@ $(document).ready( function () {
 
     // creates a new instance of an Item and adds it to the items array
     function createItemObject(room, bundle, item) {
-        itemObject = new Item(item.completed, item.name, bundle.name, room.name, item.id, item.location)
+        itemObject = new Item(item.completed, item.image, item.name, bundle.name, room.name, item.id, item.location)
         items.push(itemObject)
     };
     
